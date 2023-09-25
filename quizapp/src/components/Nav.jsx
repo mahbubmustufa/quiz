200bfdc21c2ab7946f5a8f40570407bcdba0a0fa
mahbub <mahbubmustufa@gmail.com>
2023-09-26 05:57:12 +0600

    install boyler plate

---
+++ quizapp/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Nav() {
+	return (
+		<nav className="nav">
+			<ul>
+				<li>
+					<a className="brand" href="index.html">
+						<img src=""></img>
+						<h3>Learn with sumit</h3>
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
+}
